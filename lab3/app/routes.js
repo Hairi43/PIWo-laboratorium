b3a415a12f8cadf4906491f02a06e39694b62199
Hairi <hairi@thinkpad.home>
2025-04-27 19:59:45 +0200

    tried to set up firebase

--- lab3/app/routes.js
+++
@@ -1,8 +0,0 @@
-import { index, route } from "@react-router/dev/routes";
-
-export default [
-    index("routes/Home.jsx"),
-    route("new", "routes/New.jsx"),
-    route("cart", "routes/Cart.jsx"),
-    route("login", "routes/Login.jsx"),
-];
